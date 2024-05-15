c0bc0fd231770e0c98cef95184a39bf4058c6f8a
I'm Tuan <150504127+nonotnonez@users.noreply.github.com>
2024-05-15 10:39:34 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,102 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
+# Variables
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
+variable instance_type {}
+variable ami_id {}
 
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+# VPC & Subnet
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_block
+  tags = {
+      Name = "${var.env_prefix}-vpc"
   }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t2.micro"
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  cidr_block = var.subnet_cidr_block
+  availability_zone = var.avail_zone
+  tags = {
+      Name = "${var.env_prefix}-subnet-1"
+  }
+}
+
+# Route table
+resource "aws_route_table" "myapp-route-table" {
+   vpc_id = aws_vpc.myapp-vpc.id
+
+   route {
+     cidr_block = "0.0.0.0/0"
+     gateway_id = aws_internet_gateway.myapp-igw.id
+   }
+   tags = {
+     Name = "${var.env_prefix}-rtb"
+   }
+ }
+
+ # Internet gateway
+ resource "aws_internet_gateway" "myapp-igw" {
+        vpc_id = aws_vpc.myapp-vpc.id
+    
+    tags = {
+     Name = "${var.env_prefix}-igw"
+   }
+}
+
+# Associate subnet with Route Table
+resource "aws_route_table_association" "a-rtb-subnet" {
+  subnet_id      = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-table.id
+}
+
+# Security Group
+variable my_ip {}
+
+resource "aws_security_group" "myapp-sg" {
+  name   = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
 
   tags = {
-    Name = "HelloWorld"
+    Name = "${var.env_prefix}-sg"
+  }
+}
+
+### EC2 congifure
+resource "aws_instance" "myapp-server" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+
+  key_name                    = "tf-cloud-keypair"
+  subnet_id                   = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids      = [aws_security_group.myapp-sg.id]
+  availability_zone           = var.avail_zone
+  associate_public_ip_address = true
+
+  tags    = {
+    Name = "${var.env_prefix}-server"
   }
 }
